24d6486c4c949119355c76c4fb84cbfb284e1841
Tsukanov Alexey <tsukanov.alexey@mail.ru>
2024-09-25 23:52:13 +0300

    translate validation errors in russian

---
+++ lang/ru/validation.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'min' => [
+        'string' => 'Поле :attribute должно содержать как минимум :min символа(-ов)',
+    ],
+    'required' => 'Поле :attribute обязательное.',
+
+    'attributes' => [
+        'query' => 'запрос',
+    ],
+];
