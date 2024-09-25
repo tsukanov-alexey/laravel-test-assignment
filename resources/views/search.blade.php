bb19f39a73f77ea62dff7deaaa82f3a0391cae67
Tsukanov Alexey <tsukanov.alexey@mail.ru>
2024-09-25 23:36:10 +0300

    show validation errors

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -11,13 +11,16 @@
             <div class="bg-white shadow-md rounded-lg p-6">
                 <form id="search-form" action="{{ route('search') }}">
                     <div class="flex">
-                        <input value="{{ isset($query) ? $query : '' }}"
+                        <input value="{{ old('query', isset($query) ? $query : '') }}"
                             type="text" name="query" id="query" placeholder="Введите имя или его часть..." required
                             class="w-full py-2 mr-2 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500">
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             Найти
                         </button>
                     </div>
+                    @error('query')
+                        <p class="text-red-500 mt-1 text-center">{{ $message }}</p>
+                    @enderror
                 </form>
 
                 @if (isset($users))
