1905196b5f65f30be5d3190474b015da3aa76e7d
Tsukanov Alexey <tsukanov.alexey@mail.ru>
2024-09-25 23:05:55 +0300

    save query in search bar

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -11,7 +11,8 @@
             <div class="bg-white shadow-md rounded-lg p-6">
                 <form id="search-form" action="{{ route('search') }}">
                     <div class="flex">
-                        <input type="text" name="query" id="query" placeholder="Введите имя или его часть..." required
+                        <input value="{{ isset($query) ? $query : '' }}"
+                            type="text" name="query" id="query" placeholder="Введите имя или его часть..." required
                             class="w-full py-2 mr-2 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500">
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             Найти
